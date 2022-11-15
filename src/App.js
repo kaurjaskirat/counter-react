743bcf0184e8e5ab77a6555455c0501fb3cbbe9f
unknown <jaskirat2357@gmail.com>
2022-11-15 22:07:31 +0530

    react1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+const App = () => {
+  const[text,settext]=useState(0)
+  const inval=()=>{
+    settext(text+1)
+  }
+  const decval=()=>{
+    settext(text-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center py-8'>
+      <style>{'body{background-color:pink}'}</style>
+      <h1 style={{margin:"2%",textDecoration:"underline"}}>COUNTER</h1>
+      <button onClick={inval}>INCREAMENT</button>
+      <h1 style={{margin:"4%",fontSize:"450%"}}>{text}</h1>
+      <button onClick={decval}>DECREAMENT</button>
+
     </div>
   );
 }
